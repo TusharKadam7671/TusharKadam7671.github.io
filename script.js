8bc3045e74998f8c5e83aa5b0c43283df53dd986
TusharKadam7671 <kadamtushar080@gmail.com>
2022-11-15 20:15:16 +0530

    updated

--- script.js
+++ script.js
@@ -1,78 +1,68 @@
-$(document).ready(function () {
-  $(window).scroll(function () {
-    // sticky navbar on scroll script
-    if (this.scrollY > 20) {
-      $(".navbar").addClass("sticky");
-    } else {
-      $(".navbar").removeClass("sticky");
-    }
-
-    // scroll-up button show/hide script
-    if (this.scrollY > 500) {
-      $(".scroll-up-btn").addClass("show");
-    } else {
-      $(".scroll-up-btn").removeClass("show");
-    }
+$(document).ready(function(){
+  $(window).scroll(function(){
+      if(this.scrollY > 20){
+          $('.navbar').addClass("sticky");
+      }
+      else{
+          $('.navbar').removeClass("sticky");
+      }
+      // if(this.scrolly > 500){
+      //     $('.scroll-up').addClass("show");
+      // }
+      // else{
+      //     $('.scroll-up').removeClass("show");
+      // }
   });
 
-  // slide-up script
-  $(".scroll-up-btn").click(function () {
-    $("html").animate({ scrollTop: 0 });
-    // removing smooth scroll on slide-up button click
-    $("html").css("scrollBehavior", "auto");
+
+//    typing animation script 
+
+  //   let typed = new Typed(".typing",{
+  //     strings: [" Java Backend Developer"],
+  //     typeSpeed: 100,
+  //     backSpeed: 60,
+  //     loop: true
+  //   })
+
+
+  //   let typed2 = new Typed(".typing2",{
+  //     strings: ["Java","SpringBoot","MYSQL","Hibernate"],
+  //     typeSpeed: 150,
+  //     backSpeed: 60,
+  //     loop: true
+  //   })
+
+  // slide up script 
+
+  $('.scroll-up').click(function(){
+      $('html').animate({scrollToP: 0});
   });
 
-  $(".navbar .menu li a").click(function () {
-    // applying again smooth scroll on menu items click
-    $("html").css("scrollBehavior", "smooth");
-  });
 
-  // toggle menu/navbar script
-  $(".menu-btn").click(function () {
-    $(".navbar .menu").toggleClass("active");
-    $(".menu-btn i").toggleClass("active");
-  });
-
-  // typing text animation script
-  var typed = new Typed(".typing", {
-    strings: ["Back-End Developer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true,
-  });
-
-  var typed = new Typed(".typing-2", {
-    strings: ["Back-End Developer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true,
-  });
-
-  // owl carousel script
-  $(".carousel").owlCarousel({
-    margin: 20,
-    loop: true,
-    autoplay: true,
-    autoplayTimeOut: 2000,
-    autoplayHoverPause: true,
-    responsive: {
-      0: {
-        items: 1,
-        nav: false,
-      },
-      600: {
-        items: 2,
-        nav: false,
-      },
-      // 1000: {
-      //   items: 3,
-      //   nav: false,
-      // },
-    },
-  });
-
+  
+$('.menu-btn').click(function(){
+  $('.navbar .menu').toggleClass("active")
+})
 
 });
+
+
+function git1(){
+  window.location.href = "https://github.com/TusharKadam7671/industrious-summer-462"
+  window.target = "_blank"
+}
+
+function deploy1(){
+  window.location.href = "https://github.com/TusharKadam7671/industrious-summer-462"
+}
+
+function git2(){
+  window.location.href = "https://github.com/TusharKadam7671/foregoing-tin-1857"
+}
+
+function deploy2(){
+  window.location.href = "https://dermstore-project-clone.netlify.app/"
+}
 
 GitHubCalendar(".calendar", "TusharKadam7671");
 // or enable responsive functionality
